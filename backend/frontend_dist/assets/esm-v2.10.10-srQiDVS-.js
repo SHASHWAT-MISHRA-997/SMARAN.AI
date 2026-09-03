63e1cb38ea94afabeb810c8074379d45779a365e
Shashwat Mishra <shashwatmishra062@gmail.com>
2026-09-03 16:38:05 +0530

    release: 2.10.10

---
+++ backend/frontend_dist/assets/esm-v2.10.10-srQiDVS-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-v2.10.10-DomhrLur.js","assets/dist-v2.10.10-Do0m8ifp.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./index-v2.10.10-Dt7ztrpe.js";import{n as t}from"./dist-v2.10.10-Do0m8ifp.js";var n=t(`SpeechRecognition`,{web:()=>e(()=>import(`./web-v2.10.10-DomhrLur.js`).then(e=>new e.SpeechRecognitionWeb),__vite__mapDeps([0,1]))});export{n as SpeechRecognition};
